384581982e6eac20dae4aa6a0d32500e55590e9d
Батраз Секинаев <bsekinaev@users.noreply.github.com>
2026-09-24 02:53:08 +0300

    Fix BusyBox awk MAC validation

--- src/usr/lib/home-gateway/redmi.sh
+++ src/usr/lib/home-gateway/redmi.sh
@@ -18,10 +18,21 @@ hg_redmi_is_ipv4() {
 hg_redmi_is_mac() {
     value="$1"
 
-    printf '%s\n' "$value" | awk '
-        BEGIN { IGNORECASE = 1 }
-        $0 ~ /^([0-9a-f]{2}:){5}[0-9a-f]{2}$/ { exit 0 }
-        { exit 1 }
+    printf '%s\n' "$value" | awk -F: '
+        NF != 6 {
+            exit 1
+        }
+
+        {
+            for (i = 1; i <= 6; i++) {
+                if ($i !~ /^[0-9A-Fa-f][0-9A-Fa-f]$/)
+                    exit 1
+            }
+        }
+
+        END {
+            exit 0
+        }
     '
 }
 
